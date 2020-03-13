2ba0d4d8b4c5c6957274e844ec83f41dbe1bf0d6
TidharWienreb <62143972+TidharWienreb@users.noreply.github.com>
2020-03-13 17:41:53 +0200

    Add Mat Module
    - validate successfully use matModule in calendar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { CalendarModule } from "./features/calendar/calendar.module";
 import { AppComponent } from "./app.component";
+import { MaterialModule } from "./shared/material/mat.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CalendarModule],
+  imports: [BrowserModule, CalendarModule, MaterialModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/features/calendar/day/calendar-day.component.ts
+++ src/app/features/calendar/day/calendar-day.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, ChangeDetectionStrategy } from "@angular/core";
 import { IDay } from "./model/day.interface";
 import { IEvent } from "../../event/model/event.interface";
 
 @Component({
   selector: "calendar-day",
-  templateUrl: "./calendar-day.component.html"
+  templateUrl: "./calendar-day.component.html",
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CalendarDayComponent implements IDay {
   public dayNumber: number;
